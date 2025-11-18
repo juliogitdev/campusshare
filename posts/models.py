d43204ad92a4dbd9d8ca77f7de36cb9ae95e6020
Júlio Vieira <juliovieira035@gmail.com>
2025-11-17 23:02:18 -0300

    model creation and migrations done

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,45 @@
+# Create your models here.
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tag(models.Model):
+    nome_tag = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nome_tag
+
+class Postagem(models.Model):
+    #(1,N) - Um usuário publica várias postagens
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='postagens')
+    
+
+    texto_conteudo = models.TextField(verbose_name="Conteúdo")
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+
+    #(N,N) - Postagem possui Tags
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    #(N,N) - Usuário curte Postagens
+    likes = models.ManyToManyField(User, related_name='postagens_curtidas', blank=True)
+
+    def __str__(self):
+        return f"Post de {self.autor.username} em {self.data_publicacao}"
+
+    # Método para contar curtidas
+    def total_likes(self):
+        return self.likes.count()
+
+class Comentario(models.Model):
+    #(1,N) - Usuário escreve comentários
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # O comentário precisa pertencer a uma postagem específica.
+    postagem = models.ForeignKey(Postagem, on_delete=models.CASCADE, related_name='comentarios')
+    
+    
+    texto_comentario = models.TextField(verbose_name="Comentário")
+    data_comentario = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentário de {self.autor.username} na postagem {self.postagem.id}"
--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Perfil(models.Model):
+    #(1,1) - "Um usuário possui um perfil"
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
+
+    instituicao = models.CharField(max_length=150, blank=True, null=True)
+    curso = models.CharField(max_length=100, blank=True, null=True)
+    biografia = models.TextField(blank=True, null=True)
+
+    nome_exibicao = models.CharField(max_length=50, blank=True, null=True)
+
+    foto = models.ImageField(upload_to='perfil_fotos/', blank=True, null=True)
+
+    def __str__(self):
+        return f"Perfil de {self.usuario.username}"
