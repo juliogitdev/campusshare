b241776527c944c078d38d8c1a76b0d69c4cc43f
Júlio Vieira <juliovieira035@gmail.com>
2025-11-17 22:57:45 -0300

    Revert "Add models classes and do migrations"
    This reverts commit 455a98d4c55e3e90e17de3068f49cacb2e3320d6.

--- campusshare/urls.py
+++ campusshare/urls.py
@@ -16,9 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import pagina_inicial
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', pagina_inicial)
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def pagina_inicial(request):
-    return HttpResponse("<h1> Bem-vindo à Rede Social Universitária!</h1>")
 # Create your views here.
