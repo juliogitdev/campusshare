ac3095fe9db005a3859cd7794153ac8aed4210c1
Júlio Vieira <juliovieira035@gmail.com>
2025-12-03 11:16:06 -0300

    Update app usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("<h1> Bem vindo a rota de usuarios</h1>")
