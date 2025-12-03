f2841ab5e093057554623e53af76ebf55d19ace7
Júlio Vieira <juliovieira035@gmail.com>
2025-12-03 11:16:17 -0300

    update app posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Postagem, Comentario, Tag 
 
-# Register your models here.
+@admin.register(Postagem)
+class PostagemAdmin(admin.ModelAdmin):
+    list_display = ('texto_conteudo', 'autor', 'data_publicacao') 
+
+
+@admin.register(Comentario)
+class ComentarioAdmin(admin.ModelAdmin):
+    list_display = ('autor', 'postagem', 'data_comentario')
+
+admin.site.register(Tag)
